4df8da957aa3a0a5dba86d7f9bfe25ef37fa4964
andrey12v <andrey12v@gmail.com>
2016-10-06 23:22:51 -0500

    Update ProxyServer.java

--- Proxy/src/proxy/ProxyServer.java
+++ Proxy/src/proxy/ProxyServer.java
@@ -1,15 +1,6 @@
 package proxy;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author eihabsalah
- */
 
 import java.net.*;
 import java.io.*;
